2098e9928a4dac4eff3b43c3173e412608f0282a
Dennis Andrew <Dennis.Andrew@rootquotient.com>
2026-05-04 21:58:01 +0530

    basics of js

---
+++ week_2/script.js
@@ -0,0 +1,4 @@
+let numberA=prompt("Enter the first number:");
+let numberB=prompt("Enter the second number:");
+let sum=Number(numberA)+Number(numberB);
+alert("The sum of the two numbers is: "+sum);
