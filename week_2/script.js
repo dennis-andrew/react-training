227f536c60395e7f4fefaf527c99a413bef738df
Dennis Andrew <Dennis.Andrew@rootquotient.com>
2026-05-06 16:23:51 +0530

    created a basic to-do app for dom manipulation

--- week_2/script.js
+++ week_2/script.js
@@ -1,4 +1,34 @@
-let numberA=prompt("Enter the first number:");
-let numberB=prompt("Enter the second number:");
-let sum=Number(numberA)+Number(numberB);
-alert("The sum of the two numbers is: "+sum);
+const todoList = document.querySelector("#todo-list");
+const addButton = document.querySelector("#add-button");
+const deleteButton = document.querySelector("#delete-button");
+const inputField = document.querySelector("#new-todo");
+
+addButton.addEventListener("click", function(){
+    if(inputField.value.length>0){
+        const newItem= document.createElement("li");
+        const label=document.createElement("label");
+        const input= document.createElement("input");
+        input.type="checkbox";
+        label.append(input);
+        label.append(` ${inputField.value}`);
+        newItem.appendChild(label);
+        todoList.appendChild(newItem);
+        inputField.value = "";
+    }
+});
+
+deleteButton.addEventListener("click",function(){
+    const checkedBoxes = todoList.querySelectorAll('input[type="checkbox"]:checked');
+
+    checkedBoxes.forEach(function (checkbox) {
+        checkbox.closest("li").remove();
+    });
+});
+
+todoList.addEventListener("change", function(e){
+    if(e.target.checked){
+        e.target.parentElement.style.textDecoration = "line-through";
+    } else {
+        e.target.parentElement.style.textDecoration = "none";
+    }
+});
